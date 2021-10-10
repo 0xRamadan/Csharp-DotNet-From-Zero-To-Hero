ada990118c9f580e4cc675b6ae85c745beae3c57
Mahmoud Abdallah <mah64725@gmail.com>
2021-10-10 19:32:55 +0200

    Ep016 working with Finalizer

---
+++ Ep016_OPP_Finalizer/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ep016_OPP_Finalizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Garbge Collector: Automatic Memory Management
+            /*
+             * When Object get Disposed(Destroyed)??
+             * 1. End of Program Execution (implicit)
+             * 2. Memory Full (implicit)
+             * 3. Garbge Collector (explict)
+             */
+
+            {
+                // scope 1
+                var p = new Person();
+                p.Name = "Ali";
+            }
+
+            MakeSomeGarbge();
+            Console.WriteLine($"Memory before collection: {GC.GetTotalMemory(false)}");
+            GC.Collect();
+            Console.WriteLine($"Memory after collection: {GC.GetTotalMemory(true)}");
+            Console.ReadKey();
+        }
+
+        static void MakeSomeGarbge()
+        {
+            Version v;
+            for (int i = 0; i < 1000; i++)
+            {
+                v = new Version();
+            }
+        }
+    }
+
+    class Person
+    {
+        // property
+        public string Name { get; set; }
+
+
+        // constructor
+        public Person()
+        {
+            Console.WriteLine("this is a constructor");
+        }
+
+        // Deconstructor:  be called when the object disposed/ destroyed
+        ~Person()
+        {
+            Console.WriteLine("this is a constructor");
+        }
+    }
+}
---
+++ Ep017_OOP_NestedTypes/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ep017_OOP_NestedTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                NestedTypes: it is a type defined within a class(type).
+            
+             */
+
+            Employee e1 = new Employee();
+            Console.WriteLine(e1.EmployeeInsurance.CompanyName);
+            Console.ReadKey();
+        }
+    }
+
+
+    // Nested Type 
+    // it is good for 
+    // 1. does not pollute the golbal scope.
+    // 2. the methods of B implicity have access to private members
+
+    class A
+    {
+        private int x;
+
+        class B
+        {
+            // method
+            void someMethod()
+            {
+                A a = new A();
+                // here, you can access private members in this class, as long as this class within the parent(container) class A.
+                a.x = 10;
+
+            }
+        }
+    }
+
+    // another example 
+
+    // Composition: is type of relationship between Classes -- (Car has a Motor) 
+    class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Insurance EmployeeInsurance { get; set; }
+
+        public Employee() =>
+            EmployeeInsurance = new Insurance { PolicyId = -1, CompanyName = "N/A" };
+    }
+
+    class Insurance
+    {
+        public int PolicyId { get; set; }
+        public string CompanyName { get; set; }
+    }
+
+    class Deparment
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
