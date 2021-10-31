2fdad44bf51f0951667f222816b96ff56a957820
Mahmoud Abdallah <mah64725@gmail.com>
2021-10-31 11:24:27 +0200

    woking with Enum

--- Ep020_Enum_DataType/Program.cs
+++ Ep020_Enum_DataType/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Ep20_Enum_DataType
+namespace Ep020_Enum_DataType
 {
     class Program
     {
--- Ep020_Enum_DataType/Properties/AssemblyInfo.cs
+++ Ep020_Enum_DataType/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Ep20_Enum_DataType")]
+[assembly: AssemblyTitle("Ep020_Enum_DataType")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
